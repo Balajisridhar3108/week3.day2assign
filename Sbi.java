506b4cab0b242077e4c5da4ac729e15ee8e34294
Balajisridhar3108 <92989836+Balajisridhar3108@users.noreply.github.com>
2022-02-22 20:08:37 +0530

    Add files via upload

---
+++ Sbi.java
@@ -0,0 +1,13 @@
+package week3.day2;
+
+public abstract class Sbi implements Cibil , PNB {
+ public void ITLoan() { 
+	 System.out.println("it loan");
+ } 
+ public abstract void bankBalance();
+	 
+	 public  abstract void maximumLoanAmount();
+	 
+ 
+
+}
