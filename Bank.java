c45e16c1617ef9acd4204cca2c6a393ac71342bf
Balajisridhar3108 <92989836+Balajisridhar3108@users.noreply.github.com>
2022-02-22 20:09:00 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,42 @@
+package week3.day2;
+
+public class Bank extends Sbi {
+
+	public void cibilScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void creditScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void minimumBalance() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bankBalance() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void maximumLoanAmount() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		Bank bk = new Bank();
+		bk.cibilScore();
+		bk.bankBalance();
+		bk.creditScore();
+		bk.ITLoan();
+		bk.maximumLoanAmount();
+		bk.minimumBalance();
+		
+	}
+}
